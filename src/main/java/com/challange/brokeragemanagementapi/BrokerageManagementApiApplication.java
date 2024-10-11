13fdddcf5669ec9af0ffc482a26bbb152d756407
ugurarabaci <ugurarabaci0209@gmail.com>
2024-10-11 23:45:14 +0300

    adds: maven pom.xml for project setup

---
+++ src/main/java/com/challange/brokeragemanagementapi/BrokerageManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.challange.brokeragemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerageManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerageManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challange/brokeragemanagementapi/BrokerageManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challange.brokeragemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerageManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
