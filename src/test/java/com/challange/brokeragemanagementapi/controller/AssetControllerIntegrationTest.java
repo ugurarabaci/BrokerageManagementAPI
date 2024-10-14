34503a153f03f60874ead8b0416cf2573e09dbe3
ugurarabaci <ugurarabaci0209@gmail.com>
2024-10-14 17:18:28 +0300

    changes: integration test fix

--- src/test/java/com/challange/brokeragemanagementapi/controller/AssetControllerIntegrationTest.java
+++ src/test/java/com/challange/brokeragemanagementapi/controller/AssetControllerIntegrationTest.java
@@ -9,16 +9,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class AssetControllerIntegrationTest {
 
     @Autowired
